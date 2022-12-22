6614a1c8880bcf6b9f31f388577653294c5c079a
kdw6052 <kdw6052@naver.com>
2022-12-22 21:40:46 +0900

    12일차 : university db 및 샘플데이터 추가

--- 11일차/UNIVERSITY.sql
+++ 11일차/UNIVERSITY.sql
@@ -1,151 +1,154 @@
-﻿CREATE TABLE `STUDENT` (
-	`ST_NUM`	VARCHAR(15)	NOT NULL,
-	`ST_NAME`	VARCHAR(20)	NULL,
-	`ST_SEMESTER`	INT NOT NULL DEFAULT 1	NULL,
-	`ST_STATE`	VARCHAR(10) NOT NULL '재학'	NULL	DEFAULT '재학',
-	`ST_PR_NUM`	INT	NOT NULL
+drop database if exists university;
+
+create database if not exists university;
+
+use university;
+
+DROP TABLE IF EXISTS `student`;
+
+CREATE TABLE `student` (
+	`st_num`	char(10)	NOT NULL,
+	`st_name`	varchar(20)	not NULL,
+	`st_semester`	int not null default 1,
+	`st_state`	varchar(10) not null default '재학',
+	`st_pr_num`	char(10)	NULL
 );
 
-CREATE TABLE `DEPARTEMENT` (
-	`DE_NUM`	CHAR(3)	NOT NULL,
-	`DE_NAME`	VACHAR(20)	NULL,
-	`DE_ADDRESS`	VARCHAR(50)	NULL,
-	`DE_TEL`	VARCHAR(13)	NULL,
-	`DE_PR_NUM`	CHAR(10)	NOT NULL
+DROP TABLE IF EXISTS `department`;
+
+CREATE TABLE `department` (
+	`de_num`	char(3)	NOT NULL,
+	`de_name`	varchar(20)	not NULL,
+	`de_address`	varchar(50)	not NULL,
+	`de_tel`	varchar(13)	not NULL,
+	`de_pr_num`	char(10)
 );
 
-CREATE TABLE `SCORE` (
-	`SC_NUM`	INT AUTO_INCREMENT	NOT NULL,
-	`SC_MID`	INT NOT NULL DEFAULT 0	NULL,
-	`SC_FINAL`	INT NOT NULL DEFAULT 0	NULL,
-	`SC_HOMEWORK`	INT NOT NULL DEFAULT 0	NULL,
-	`SC_ATTENDANCE`	INT NOT NULL DEFAULT 0	NULL,
-	`SC_TOTAL`	INT NOT NULL DEFAULT 0	NULL,
-	`SC_CO_NUM`	INT	NOT NULL
+DROP TABLE IF EXISTS `mojor`;
+
+CREATE TABLE `mojor` (
+	`mo_num`	int	auto_increment primary key,
+	`mo_st_num`	varchar(10)	NOT NULL,
+	`mo_de_num`	char(3)	NOT NULL,
+	`mo_state`	varchar(10)	not NULL default '전공'
 );
 
-CREATE TABLE `PROFESSOR` (
-	`PR_NUM`	CHAR(10)	NOT NULL,
-	`PR_NAME`	VARCHAR(20)	NULL,
-	`PR_STATE`	VARCHAR(10)	NULL,
-	`PR_DE_NUM`	CHAR(3)	NOT NULL,
-	`PR_TEL`	VARCHAR(13)	NULL
+DROP TABLE IF EXISTS `professor`;
+
+CREATE TABLE `professor` (
+	`pr_num`	char(10)	NOT NULL,
+	`pr_name`	varchar(20)	NULL,
+	`pr_state`	varchar(10)	NULL,
+	`pr_de_num`	char(3)	NOT NULL,
+	`pr_tel`	varchar(13)	NULL
 );
 
-CREATE TABLE `LECTURE` (
-	`LE_NUM`	INT AUTO_INCREMENT	NOT NULL,
-	`LE_NAME`	VARCHAR(30)	NULL,
-	`LE_SCHEDULE`	VARCHAR(50)	NULL,
-	`LE_POINT`	INT	NULL,
-	`LE_CLASS`	INT	NULL,
-	`LE_YEAR`	INT	NULL,
-	`LE_TERM`	INT	NULL,
-	`LE_PR_NUM`	CHAR(10)	NOT NULL
+DROP TABLE IF EXISTS `lecture`;
+
+CREATE TABLE `lecture` (
+	`le_num`	int	auto_increment primary key,
+	`le_name`	varchar(20)	not NULL,
+	`le_schedule`	varchar(50)	not NULL default '',
+	`le_point`	int	not NULL default 0,
+	`le_class`	int	not NULL default 1,
+	`le_year`	int	not NULL,
+	`le_term`	varchar(10) not null,
+	`le_pr_num`	char(10)	NOT NULL
 );
 
-CREATE TABLE `COURSE` (
-	`CO_NUM`	INT AUTO_INCREMENT	NOT NULL,
-	`CO_ST_NUM`	VARCHAR(15)	NOT NULL,
-	`CO_LE_NUM`	INT	NOT NULL,
-	`CO_TYPE`	CARCHAR(5)	NULL,
-	`CO_GRADE`	VARCHAR(3)	NULL
+DROP TABLE IF EXISTS `course`;
+
+CREATE TABLE `course` (
+	`co_num`	int	auto_increment primary key,
+	`co_st_num`	char(10)	NOT NULL,
+	`co_le_num`	int	NOT NULL,
+	`co_type`	varchar(5)	not NULL default '학점',
+	`co_grade`	varchar(5)	not NULL default ''
 );
 
-CREATE TABLE `MOJOR` (
-	`MO_NUM`	INT AUTO_INCREMENT	NOT NULL,
-	`MO_ST_NUM`	VARCHAR(15)	NOT NULL,
-	`MO_DE_NUM`	CHAR(3)	NOT NULL,
-	`MO_STATE`	VARCHAR(10)	NULL
+DROP TABLE IF EXISTS `score`;
+
+CREATE TABLE `score` (
+	`sc_num`	int	auto_increment primary key,
+	`sc_mid`	int not null default 0,
+	`sc_final`	int not null default 0,
+	`sc_homework`	int not null default 0,
+	`sc_attendance`	int not null default 0,
+	`sc_total`	int not null default 0,
+	`sc_co_num`	int	NOT NULL
 );
 
-ALTER TABLE `STUDENT` ADD CONSTRAINT `PK_STUDENT` PRIMARY KEY (
-	`ST_NUM`
+ALTER TABLE `student` ADD CONSTRAINT `PK_STUDENT` PRIMARY KEY (
+	`st_num`
 );
 
-ALTER TABLE `DEPARTEMENT` ADD CONSTRAINT `PK_DEPARTEMENT` PRIMARY KEY (
-	`DE_NUM`
+ALTER TABLE `department` ADD CONSTRAINT `PK_DEPARTMENT` PRIMARY KEY (
+	`de_num`
 );
 
-ALTER TABLE `SCORE` ADD CONSTRAINT `PK_SCORE` PRIMARY KEY (
-	`SC_NUM`
+ALTER TABLE `professor` ADD CONSTRAINT `PK_PROFESSOR` PRIMARY KEY (
+	`pr_num`
 );
 
-ALTER TABLE `PROFESSOR` ADD CONSTRAINT `PK_PROFESSOR` PRIMARY KEY (
-	`PR_NUM`
-);
-
-ALTER TABLE `LECTURE` ADD CONSTRAINT `PK_LECTURE` PRIMARY KEY (
-	`LE_NUM`
-);
-
-ALTER TABLE `COURSE` ADD CONSTRAINT `PK_COURSE` PRIMARY KEY (
-	`CO_NUM`
-);
-
-ALTER TABLE `MOJOR` ADD CONSTRAINT `PK_MOJOR` PRIMARY KEY (
-	`MO_NUM`
-);
-
-ALTER TABLE `STUDENT` ADD CONSTRAINT `FK_PROFESSOR_TO_STUDENT_1` FOREIGN KEY (
-	`ST_PR_NUM`
+ALTER TABLE `student` ADD CONSTRAINT `FK_professor_TO_student_1` FOREIGN KEY (
+	`st_pr_num`
 )
-REFERENCES `PROFESSOR` (
-	`PR_NUM`
+REFERENCES `professor` (
+	`pr_num`
 );
 
-ALTER TABLE `DEPARTEMENT` ADD CONSTRAINT `FK_PROFESSOR_TO_DEPARTEMENT_1` FOREIGN KEY (
-	`DE_PR_NUM`
+ALTER TABLE `department` ADD CONSTRAINT `FK_professor_TO_department_1` FOREIGN KEY (
+	`de_pr_num`
 )
-REFERENCES `PROFESSOR` (
-	`PR_NUM`
+REFERENCES `professor` (
+	`pr_num`
 );
 
-ALTER TABLE `SCORE` ADD CONSTRAINT `FK_COURSE_TO_SCORE_1` FOREIGN KEY (
-	`SC_CO_NUM`
+ALTER TABLE `mojor` ADD CONSTRAINT `FK_student_TO_mojor_1` FOREIGN KEY (
+	`mo_st_num`
 )
-REFERENCES `COURSE` (
-	`CO_NUM`
+REFERENCES `student` (
+	`st_num`
 );
 
-ALTER TABLE `PROFESSOR` ADD CONSTRAINT `FK_DEPARTEMENT_TO_PROFESSOR_1` FOREIGN KEY (
-	`PR_DE_NUM`
+ALTER TABLE `mojor` ADD CONSTRAINT `FK_department_TO_mojor_1` FOREIGN KEY (
+	`mo_de_num`
 )
-REFERENCES `DEPARTEMENT` (
-	`DE_NUM`
+REFERENCES `department` (
+	`de_num`
 );
 
-ALTER TABLE `LECTURE` ADD CONSTRAINT `FK_PROFESSOR_TO_LECTURE_1` FOREIGN KEY (
-	`LE_PR_NUM`
+ALTER TABLE `professor` ADD CONSTRAINT `FK_department_TO_professor_1` FOREIGN KEY (
+	`pr_de_num`
 )
-REFERENCES `PROFESSOR` (
-	`PR_NUM`
+REFERENCES `department` (
+	`de_num`
 );
 
-ALTER TABLE `COURSE` ADD CONSTRAINT `FK_STUDENT_TO_COURSE_1` FOREIGN KEY (
-	`CO_ST_NUM`
+ALTER TABLE `lecture` ADD CONSTRAINT `FK_professor_TO_lecture_1` FOREIGN KEY (
+	`le_pr_num`
 )
-REFERENCES `STUDENT` (
-	`ST_NUM`
+REFERENCES `professor` (
+	`pr_num`
 );
 
-ALTER TABLE `COURSE` ADD CONSTRAINT `FK_LECTURE_TO_COURSE_1` FOREIGN KEY (
-	`CO_LE_NUM`
+ALTER TABLE `course` ADD CONSTRAINT `FK_student_TO_course_1` FOREIGN KEY (
+	`co_st_num`
 )
-REFERENCES `LECTURE` (
-	`LE_NUM`
+REFERENCES `student` (
+	`st_num`
 );
 
-ALTER TABLE `MOJOR` ADD CONSTRAINT `FK_STUDENT_TO_MOJOR_1` FOREIGN KEY (
-	`MO_ST_NUM`
+ALTER TABLE `course` ADD CONSTRAINT `FK_lecture_TO_course_1` FOREIGN KEY (
+	`co_le_num`
 )
-REFERENCES `STUDENT` (
-	`ST_NUM`
+REFERENCES `lecture` (
+	`le_num`
 );
 
-ALTER TABLE `MOJOR` ADD CONSTRAINT `FK_DEPARTEMENT_TO_MOJOR_1` FOREIGN KEY (
-	`MO_DE_NUM`
+ALTER TABLE `score` ADD CONSTRAINT `FK_course_TO_score_1` FOREIGN KEY (
+	`sc_co_num`
 )
-REFERENCES `DEPARTEMENT` (
-	`DE_NUM`
+REFERENCES `course` (
+	`co_num`
 );
-
